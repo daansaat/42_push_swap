1766e80bca65dd7fdc1d0e907d37f6ca783fdd96
Daan Saat <daansaat@Daans-MacBook-Air.local>
2022-05-08 09:52:43 +0200

    small_sort

--- src/print_list.c
+++ src/print_list.c
@@ -20,3 +20,30 @@ void	print_list(struct node *head_a, struct node *head_b)
 	}
 	printf("\n");
 }
+
+// void    sort_3(struct node **head_a)
+// {
+//     int a;
+//     int b;
+//     int c;
+
+//     a = (*head_a)->nb;
+//     b = (*head_a)->next->nb;
+//     c = (*head_a)->next->next->nb;
+//     if (a > b && b < c && a < c)
+//         sab(head_a, 'a');
+//     else if (a > b && b < c)
+//         rab(head_a, 'a');
+//     else if (a > b && b > c)
+//     {
+//         sab(head_a, 'a');
+//         rrab(head_a, 'a');
+//     }
+//     else if (a < b && b > c && a < c)
+//     {
+//         sab(head_a, 'a');
+//         rab(head_a, 'a');
+//     }
+//     else
+//         rrab(head_a, 'a');
+// }
